e1d3e3a238b754df707f42bed01af986497fd496
Parth Aggarwal <paggarwal127@gmail.com>
2021-02-26 21:36:18 +0530

    solution to problem 2

---
+++ 2.delete userDatabase.sql
@@ -0,0 +1,7 @@
+-- Q. Write another SQL script that cleans up the user, and database created in the previous step.
+
+-- Run these scripts inside the sql prompt with any user
+-- having privilage of CREATEDB AND CREATE ROLE
+
+Drop DATABASE ipl;
+DROP USER parth;
